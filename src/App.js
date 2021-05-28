ae0a09101f1032ec73846e15baf13af0a53fbf3b
vulh1209 <vulh@ttgvn.com>
2021-05-28 14:21:20 +0700

    create app

--- src/App.js
+++ src/App.js
@@ -4,20 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form id="home" action="google.com">
+            <label>Name</label>
+            <input id="Name" className="o2o-name"/>
+            <label>Phone</label>
+            <input id="Phone" className="o2o-phone"/>
+            <label>Email</label>
+            <input id="Email" className="o2o-email"/>
+            <label>Note</label>
+            <input id="Note" className="o2o-note"/>
+            <button type="submit" id="submit">SUBMIT</button>
+        </form>
     </div>
   );
 }
